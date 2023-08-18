e60e4a91655286ba9e41d77fb580b9e6fbf737db
RosalesJhordi <1428060@senati.pe>
2023-08-17 19:25:06 -0500

    Creacion de array y añadir la opcion de elegir

--- Busqueda/src/main/java/com/mycompany/busqueda/Busqueda.java
+++ Busqueda/src/main/java/com/mycompany/busqueda/Busqueda.java
@@ -1,6 +1,7 @@
 
 package com.mycompany.busqueda;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Busqueda {
@@ -13,18 +14,26 @@ public class Busqueda {
         
         System.out.print("\nIngrese priner numero: ");
         n1 = v.nextInt();
-        System.out.print("\nIngrese segundo numero: ");
+        System.out.print("Ingrese segundo numero: ");
         n2 = v.nextInt();
-        System.out.print("\nIngrese tercer numero: ");
+        System.out.print("Ingrese tercer numero: ");
         n3 = v.nextInt();
-        System.out.print("\nIngrese cuerto numero: ");
+        System.out.print("Ingrese cuerto numero: ");
         n4 = v.nextInt();
-        System.out.print("\nIngrese quinto numero: ");
+        System.out.print("Ingrese quinto numero: ");
         n5 = v.nextInt();
         
         int[] numeros ={n1,n2,n3,n4,n5};
-    }
         
+        System.out.print("\nOdernar de: \n1: Menor a mayor \n2: Mayor a menor \n Opcion: ");
+        int op = v.nextInt();
         
+        if(op == 1){
+            Arrays.sort(numeros);
+            System.out.println("\t MENOR A MAYOR \n Numeros: "+ Arrays.toString(numeros));
+        }
+        else if(op == 2){
+            
+        }
     }
 }
