25421b587dc4bf5e50aa660420a0f524f800dfc5
RosalesJhordi <1428060@senati.pe>
2023-08-17 19:00:17 -0500

    Comenzamos

---
+++ Busqueda/src/main/java/com/mycompany/busqueda/Busqueda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.busqueda;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Busqueda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
