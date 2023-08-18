d23ca5f608728fb2fd18e4f7b88a49dab9b24323
RosalesJhordi <1428060@senati.pe>
2023-08-17 19:12:09 -0500

    Creacion de prints y obtener los numeros ingresados

--- Busqueda/src/main/java/com/mycompany/busqueda/Busqueda.java
+++ Busqueda/src/main/java/com/mycompany/busqueda/Busqueda.java
@@ -11,6 +11,18 @@ public class Busqueda {
         Scanner v = new Scanner(System.in);
         int n1,n2,n3,n4,n5;
         
+        System.out.print("\nIngrese priner numero: ");
+        n1 = v.nextInt();
+        System.out.print("\nIngrese segundo numero: ");
+        n2 = v.nextInt();
+        System.out.print("\nIngrese tercer numero: ");
+        n3 = v.nextInt();
+        System.out.print("\nIngrese cuerto numero: ");
+        n4 = v.nextInt();
+        System.out.print("\nIngrese quinto numero: ");
+        n5 = v.nextInt();
+        
+        int[] numeros ={n1,n2,n3,n4,n5};
     }
         
         
